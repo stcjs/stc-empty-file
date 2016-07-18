9189afd8d4001c6f75389a314676d030d283c74d
lushijie <lushijie1218@126.com>
2016-07-18 11:25:44 +0800

    Revert "init stc-move"
    This reverts commit 379c1064a252e50f236dc72997227eae5fee4e64.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# stc-move
+# stc-empty-file
 
-Stc plugin for moving file or dir
+Stc plugin for check empty file
 
+## Install
+
+```sh
+npm install stc-empty-file
+```
+
+## How to use
+
+```
+var emptyfile = require('stc-empty-file');
+
+//check empty file
+stc.lint({
+  emptyfile: {plugin: emptyfile, include: /\.*$/}
+});
+```
--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ let options = null;
 /**
  * Use ESlint to verify code
  */
-export default class MovePlugin extends Plugin {
+export default class EmptyFilePlugin extends Plugin {
   /**
    * run
    */
   async run(){
-
+    let content = await this.getContent('utf8');
+    return {isEmpty:content.length === 0};
   }
   /**
    * update
    */
   update(data){
-
+      if(data.isEmpty){
+        this.error('File can\'t be empty');
+      }
   }
   /**
    * use cluster
